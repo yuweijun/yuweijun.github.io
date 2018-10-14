bfe3f69c24b5d4416d152f2f0a9669bf2d04e7b7
yuweijun <test.yu@gmail.com>
2018-10-14 10:46:15 +0800

    add article clean js

--- js/clean.article-1.0.js
+++ js/clean.article-1.0.js
@@ -5,7 +5,7 @@
         var elem = article;
         while (elem.length && elem.get(0).tagName.toUpperCase() !== 'BODY') {
             elem.width(1024)
-                .css({padding: 0})
+                .css({padding: 5})
                 .css({margin: '0 auto'})
                 .css({minWidth: 1024})
                 .siblings().remove();
