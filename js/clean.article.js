1bf0cb7ab894998de1bec32ae5385752bc62906e
yuweijun <test.yu@gmail.com>
2018-10-14 11:58:26 +0800

    update clean.article.js

--- js/clean.article.js
+++ js/clean.article.js
@@ -6,6 +6,7 @@
         while (elem.length && elem.get(0).tagName.toUpperCase() !== 'BODY') {
             elem.width(1024)
                 .css({minWidth: 1024})
+                .css({padding: 0})
                 .css({margin: '0 auto'})
                 .siblings().remove();
             elem = elem.parent();
