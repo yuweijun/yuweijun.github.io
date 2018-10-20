98682fb06afe488b6525e35a3f04b7e99cf8e40e
yuweijun <yuweijun@live.com>
2018-10-20 23:37:17 +0800

    update d.js

--- js/d.js
+++ js/d.js
@@ -1,31 +1,23 @@
 (function() {
 
-    var methods = {
+    var element = {
         siblings: function() {
-            var matched = [];
-            this.forEach(function(elem) {
-                matched = matched.concat([...elem.parentElement.children].filter(c => c.nodeType == 1 && c != elem));
-            });
-
-            return matched;
+            return [...this.parentElement.children].filter(c => c.nodeType == 1 && c != this);
         },
-
         attr: function(attributes) {
-            // Don't get/set attributes on text, comment and attribute nodes
             if (this.nodeType === 3 || this.nodeType === 8 || this.nodeType === 2) {
                 return this;
             }
 
-            if (attributes instanceof Object) {
+            if (typeof attributes === 'string') {
+                return this.getAttribute(attributes);
+            } else {
                 for (var key in attributes) {
                     this.setAttribute(key, attributes[key]);
                 }
                 return this;
-            } else {
-                return this.getAttribute(attributes);
             }
         },
-
         css: function(styles) {
             if (this.nodeType === 1) {
                 for (var key in styles) {
@@ -35,35 +27,25 @@
 
             return this;
         },
-
         parents: function() {
-            var matched = [];
-            this.forEach(function(elem) {
-                while (elem) {
-                    if (elem.nodeType === 9) {
-                        break;
-                    }
-
-                    if (elem.nodeType === 1) {
-                        if (matched.indexOf(elem) === -1) {
-                            matched.push(elem);
-                        }
-                    }
-
-                    elem = elem.parentNode;
+            var matched = [],
+                elem = this;
+            while (elem) {
+                if (elem.nodeType === 9) {
+                    break;
                 }
-            });
+
+                if (elem.nodeType === 1) {
+                    if (matched.indexOf(elem) === -1) {
+                        matched.push(elem);
+                    }
+                }
+
+                elem = elem.parentNode;
+            }
 
             return matched;
         },
-
-        closest: function(selector) {
-            var matched = [];
-            this.forEach(elem => matched.push(elem.closest(selector)));
-
-            return matched;
-        },
-
         hasClass: function(selector) {
             var classes = this.getAttribute && this.getAttribute("class") || "",
                 className = " " + selector + " ";
@@ -73,67 +55,13 @@
 
             return false;
         },
-
-        readable: function() {
-            if (this.length === 0) return;
-
-            var parents = this.parents();
-
-            this.forEach(function(elem) {
-                while (elem && elem.tagName.toUpperCase() !== 'BODY') {
-                    var $elem = $(elem);
-                    $elem.css({
-                        padding: 0,
-                        margin: '0 auto',
-                        minWidth: '1024px',
-                        width: '1024px',
-                        position: 'static'
-                    });
-
-                    $elem.siblings().filter(function(elem) {
-                        if (elem.tagName.toUpperCase() === 'LINK') {
-                            return false;
-                        }
-                        if (parents.indexOf(elem) > -1) {
-                            return false;
-                        }
-                        return true;
-                    }).remove();
-
-                    elem = elem.parentNode;
-                }
-            });
-
-            $(this).css({
-                padding: '15px',
-                boxSizing: 'border-box'
-            });
-
-            var body = document.body,
-                html = document.documentElement;
-            document.body.style.height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-
-            return this;
-        },
-
         prepend: function() {
             [...arguments].forEach(elem => {
                 $(elem).forEach(e => this.insertBefore(e, this.firstElementChild));
             });
 
             return this;
-        },
-
-        remove: function() {
-            this.forEach(function(elem) {
-                if (elem instanceof Node) {
-                    elem.remove();
-                }
-            });
-
-            return this;
         }
-
     };
 
     // transform object of Node/NodeList/Array to enhanced Array instance
@@ -195,34 +123,44 @@
         };
         attr.remove = {
             value: function() {
-                var array = methods.remove.apply(this, arguments);
-                return $(array);
-            },
-            enumerable: false
-        };
-        attr.readable = {
-            value: function() {
-                return methods.readable.apply(this, arguments);
+                this.forEach(function(elem) {
+                    if (elem.nodeType === 1) {
+                        elem.remove();
+                    }
+                });
+                return this;
             },
             enumerable: false
         };
         attr.siblings = {
             value: function() {
-                var array = methods.siblings.apply(this, arguments);
+                var array = [];
+                this.forEach(function(elem) {
+                    array = array.concat(element.siblings.apply(elem, arguments));
+                });
+
                 return $(array);
             },
             enumerable: false
         };
         attr.parents = {
             value: function() {
-                var array = methods.parents.apply(this, arguments);
+                var array = [];
+                this.forEach(function(elem) {
+                    element.parents.apply(elem, arguments).forEach(function(p) {
+                        if (array.indexOf(p) === -1) {
+                            array.push(p);
+                        }
+                    });
+                });
                 return $(array);
             },
             enumerable: false
         };
         attr.closest = {
-            value: function() {
-                var array = methods.closest.apply(this, arguments);
+            value: function(selector) {
+                var array = [];
+                this.forEach(elem => array.push(elem.closest(selector)));
                 return $(array);
             },
             enumerable: false
@@ -230,7 +168,7 @@
         attr.hasClass = {
             value: function() {
                 this.forEach(elem => {
-                    if (!methods.hasClass.apply(elem, arguments)) return false;
+                    if (!element.hasClass.apply(elem, arguments)) return false;
                 });
                 return true;
             },
@@ -239,12 +177,15 @@
         attr.attr = {
             value: function() {
                 if (arguments.length) {
-                    if (arguments[0] instanceof Object) {
-                        this.forEach(elem => methods.attr.apply(elem, arguments));
-                    } else {
+                    if (typeof arguments[0] === 'string') {
                         var values = [];
-                        this.forEach(elem => values.push(methods.attr.apply(elem, arguments)));
+                        this.forEach(elem => values.push(element.attr.apply(elem, arguments)));
+                        if (values.length === 1) {
+                            return values[0];
+                        }
                         return values;
+                    } else {
+                        this.forEach(elem => element.attr.apply(elem, arguments));
                     }
                 }
                 return this;
@@ -253,14 +194,82 @@
         };
         attr.css = {
             value: function() {
-                this.forEach(elem => methods.css.apply(elem, arguments));
+                this.forEach(elem => element.css.apply(elem, arguments));
                 return this;
             },
             enumerable: false
         };
+        // attr.after
+        // attr.before
+        // attr.append
+        // attr.parent
+        // attr.addClass
+        // attr.removeClass
+        // attr.hide
+        // attr.show
+        // attr.val
         attr.prepend = {
             value: function() {
-                this.forEach(elem => methods.prepend.apply(elem, arguments));
+                this.forEach(elem => element.prepend.apply(elem, arguments));
+                return this;
+            },
+            enumerable: false
+        };
+        attr.querySelectorAll = {
+            value: function(selector) {
+                if (typeof selector === 'string') {
+                    var array = [];
+                    this.forEach(function(elem) {
+                        var subs = elem.querySelectorAll(selector);
+                        array.push(...subs);
+                    });
+                    return $(array);
+                } else {
+                    return $(selector);
+                }
+            },
+            enumerable: false
+        };
+        attr.readable = {
+            value: function() {
+                if (this.length === 0) return;
+
+                var parents = this.parents();
+
+                this.forEach(function(elem) {
+                    while (elem && elem.tagName.toUpperCase() !== 'BODY') {
+                        var $elem = $(elem);
+                        $elem.css({
+                            padding: 0,
+                            margin: '0 auto',
+                            minWidth: '1024px',
+                            width: '1024px',
+                            position: 'static'
+                        });
+
+                        $elem.siblings().filter(function(elem) {
+                            if (elem.tagName.toUpperCase() === 'LINK') {
+                                return false;
+                            }
+                            if (parents.indexOf(elem) > -1) {
+                                return false;
+                            }
+                            return true;
+                        }).remove();
+
+                        elem = elem.parentNode;
+                    }
+                });
+
+                $(this).css({
+                    padding: '15px',
+                    boxSizing: 'border-box'
+                });
+
+                var body = document.body,
+                    html = document.documentElement;
+                document.body.style.height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+
                 return this;
             },
             enumerable: false
@@ -283,7 +292,6 @@
         },
         enumerable: false
     });
-
 
     Object.defineProperty(NodeList.prototype, '$', {
         get() {
